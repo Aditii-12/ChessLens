69eade5e8baec1cc6754de9ae36bef66d1103d1a
Aditii-12 <sahuaditi1202@gmail.com>
2026-01-21 15:15:40 +0530

    add FEN generation module skeleton

---
+++ fen_gen.py
@@ -0,0 +1,12 @@
+"""
+fen_gen.py
+
+Converts board/screenshot data into FEN notation.
+Implementation will be added incrementally.
+"""
+
+def generate_fen():
+    """
+    Generate FEN string from detected board state.
+    """
+    return ""
