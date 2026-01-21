cf56d185a48d341be53b62c3739b7034924cd9d2
Aditii-12 <sahuaditi1202@gmail.com>
2026-01-21 15:17:11 +0530

    add chess engine module skeleton

---
+++ engine.py
@@ -0,0 +1,11 @@
+"""
+engine.py
+
+Handles chess engine (Stockfish) interaction for ChessLens.
+"""
+
+def analyze_position(fen):
+    """
+    Analyze a given FEN position and return suggested moves.
+    """
+    return []
