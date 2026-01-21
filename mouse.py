c354d94dae8f8739fd4776b1ce4a4013cc7de881
Aditii-12 <sahuaditi1202@gmail.com>
2026-01-21 15:15:03 +0530

    add mouse control module skeleton

---
+++ mouse.py
@@ -0,0 +1,12 @@
+"""
+mouse.py
+
+Handles mouse and gesture-related actions for ChessLens.
+Implementation will be added incrementally.
+"""
+
+def init_mouse_controller():
+    """
+    Initialize mouse / gesture control.
+    """
+    pass
