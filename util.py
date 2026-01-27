3f5c317a4cad05ed14200743e8bbe6890e7a974b
Aditii-12 <sahuaditi1202@gmail.com>
2026-01-27 18:30:15 +0530

    Clean up gesture math utilities

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import numpy as np
 def get_angle(a, b, c):
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(np.degrees(radians))
-    if angle > 180:
+    if angle > 180:  # keep angle within [0,180]
         angle = 360 - angle
     return angle
 
@@ -11,5 +11,4 @@ def get_distance(landmark_list):
     if len(landmark_list) < 2:
         return None
     (x1, y1), (x2, y2) = landmark_list[0], landmark_list[1]
-    return np.hypot(x2 - x1, y2 - y1)
-
+    return np.hypot(x2 - x1, y2 - y1)  # return actual pixel distance
